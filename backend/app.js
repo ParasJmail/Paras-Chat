a234fb06fa188ac8817c5457045a22a3de8ba5b0
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-06-19 22:43:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const cors = require("cors")
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
 // Using Middlewares
 app.use(express.json({ limit: "50mb" }));
+app.use(cors());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
