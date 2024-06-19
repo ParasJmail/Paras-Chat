d3448230e65031e1821c53e45f7057107713f484
ParasJmail <116335733+ParasJmail@users.noreply.github.com>
2024-06-19 23:37:11 +0530

    dotenv added

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const cors = require("cors")
+
+dotenv.config();
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
-
+const app = express();
 // Using Middlewares
+
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
